1b2ad45795a320ba190eab43ebfda9e8c9b24ffc
aniledev <elinamcgill@gmail.com>
2021-05-25 23:16:57 -0400

    FEAT: create an example Promise using an executor function

--- promises.js
+++ promises.js
@@ -1,4 +1,5 @@
 // constructing a promise using an executor function
+/*
 const executorFunction = (resolve, reject) => {
   if (someCondition) {
     resolve("I resolved!");
@@ -7,3 +8,26 @@ const executorFunction = (resolve, reject) => {
   }
 };
 const createPromise = new Promise(executorFunction);
+*/
+
+// constructing a promise using an executor function and dummy data
+const inventory = {
+  sunglasses: 1900,
+  pants: 1088,
+  bags: 1324,
+};
+
+const executor = (resolve, reject) => {
+  if (inventory.sunglasses > 0) {
+    resolve("Sunglasses order processed!");
+  } else {
+    reject("That item is sold out.");
+  }
+};
+
+const orderSunglasses = () => {
+  return new Promise(executor); // this returns a new promise; not the resolved value of the promise
+};
+const orderPromise = orderSunglasses(); // this variable saves the resolved value of the promise
+console.log(orderSunglasses); // [Function: orderSunglasses]
+console.log(orderPromise); // Promise { "Sunglasses order processed!" }
